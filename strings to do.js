2848766a80d12673a0f1e96d0fd88c9fe04b85bb
sharif721 <smuyanja11@gmail.com>
2023-07-15 05:24:33 -0700

    first commit

---
+++ strings to do.js
@@ -0,0 +1,8 @@
+// Remove Blanks
+// Create a function that, given a string, returns all of that string’s contents, but without blanks. 
+
+// Examples:
+
+// removeBlanks(" Pl ayTha tF u nkyM usi c ") => "PlayThatFunkyMusic"
+// removeBlanks("I can not BELIEVE it's not BUTTER") => "IcannotBELIEVEit'snotBUTTER"
+
